5a2186e11f8a3eb88846ad7f888f0663f02b09d8
Christoph Rüßler <christoph.ruessler@mailbox.org>
2021-03-20 15:32:59 +0100

    Fix worker not being assigned to a variable

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import { Elm } from "./src/Main.elm";
 
 const app = Elm.Main.init({ node: document.getElementById("root") });
 
-new Worker(new URL("./worker.js", import.meta.url));
+const worker = new Worker(new URL("./worker.js", import.meta.url));
 
 app.ports.sendCommand.subscribe(function (command) {
   worker.postMessage(command);
