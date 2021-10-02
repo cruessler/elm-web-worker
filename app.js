a61dd8815e8d8a15bd4ea2ba4056fe6d92410da8
Christoph Rüßler <christoph.ruessler@mailbox.org>
2021-10-02 14:14:41 +0200

    Update parcel to 2.0.0-rc.0
    - Add `type="module"` and `type: "module"`. Parcel is more strict now
      when it comes to the distinction between modules and non-modules in
      JS.

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ import { Elm } from "./src/Main.elm";
 
 const app = Elm.Main.init({ node: document.getElementById("root") });
 
-const worker = new Worker(new URL("./worker.js", import.meta.url));
+const worker = new Worker(new URL("./worker.js", import.meta.url), {
+  type: "module",
+});
 
 app.ports.sendCommand.subscribe(function (command) {
   worker.postMessage(command);
